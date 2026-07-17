87015130dad416e2b84cb52039601b9528926db8
Jensen Wu <2025141470196@stu.scu.edu.cn>
2026-07-17 20:04:34 +0800

    add gitignore to ignore build files and vscode config

--- labs/lab02/lfsr.c
+++ labs/lab02/lfsr.c
@@ -6,5 +6,16 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    unsigned b0 = *reg & 1;
+    unsigned b2 = (*reg >> 2) & 1;
+    unsigned b3 = (*reg >> 3) & 1;
+    unsigned b5 = (*reg >> 5) & 1;
+    unsigned fb = b0 ^ b2 ^ b3 ^ b5;
+
+    unsigned short res = *reg >> 1;
+
+    res = res | (fb << 15);
+
+    *reg = res;
 }
 
