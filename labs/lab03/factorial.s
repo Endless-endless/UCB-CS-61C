990aba357451813db5f99bd110ee66cf0ea0d3c5
Jensen Wu <2025141470196@stu.scu.edu.cn>
2026-07-27 23:29:24 +0800

    Lab03 完成链表map递归实验，补充实验总结summary.md

--- labs/lab03/factorial.s
+++ labs/lab03/factorial.s
@@ -22,3 +22,26 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    addi sp, sp, -12
+    sw s0, 0(sp)
+    sw s1, 4(sp)
+    sw ra, 8(sp)
+    
+    addi s0, x0, 1
+    add s1, x0, a0
+    
+loop_fact:
+    beq s1, x0, exit_fact
+    mul s0, s0, s1
+    addi s1, s1, -1
+    jal x0, loop_fact
+    
+exit_fact:
+    add a0 x0 s0
+    
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    lw ra, 8(sp)
+    addi sp, sp, 12
+    
+    jr ra
